82b489775f1155a2ee313a74f9a97f1a5434a6ae
ChizuIra <kishidq@gmail.com>
2025-11-09 02:03:12 +0100

    Le goat Hapax qui a tout refacto

---
+++ README.md
@@ -0,0 +1,8 @@
+La boule du joueur fait la course avec la boule de l'ordinateur.
+Si la boule de l'ordinateur arrive a 10 Tours avant le joueur, ce dernier perd.
+Si la boule du joueur arrive a  10 Tours en premier, le joueur a alors jusqu'a la fin de la course de l'ordinateur pour faire un maximum de Tours.
+> Chaque tours supplementaire donne +1$
+
+A chaque fin de Course le joueur fait face a une boutique, dans laquelle il peut utiliser ses $ pour acheter des bonus.
+
+Interet a la balatro + 
--- main.cpp
+++ main.cpp
@@ -1,92 +1,153 @@
 #include "./lib/raylib.h"
 #include <cstdlib>
 #include <sstream>
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <memory>
+
+constexpr unsigned int MODIF_MAX = 9;
 
 class Boule {
     private:
-        int _pos_x;
-        int _pos_y;
-        int _vit_x;
-        int _vit_y;
-        int          _alt_vitesse;
-        const int    _radius;
-        Color        _color;
-        int _score;
+	Vector2 _pos;
+	Vector2 _vit;
+        int     _alt_vitesse;
+        int     _radius;
+        Color   _color;
+        int     _score;
+
+	Vector2 get_next_pos() const {
+	    return CLITERAL(Vector2){.x=_pos.x+_vit.x*_alt_vitesse, .y=_pos.y+_vit.y*_alt_vitesse};
+	}
+
+	void check_collision(char* hit_x, char* hit_y) const {
+	    Vector2 fut = get_next_pos();
+            if(fut.x >= GetScreenWidth()) {
+                *hit_x = 1;
+            }
+	    if(fut.x < 0) {
+		*hit_x = -1;
+	    }
+	    if(fut.y >= GetScreenHeight()) {
+		*hit_y = 1;
+	    }
+	    if(fut.x < 0) {
+		*hit_y = -1;
+	    }
+	}
+
+	void roll_alt() {
+	    _alt_vitesse = (rand()%MODIF_MAX)+1;
+	}
 
     public:
         Boule(int pos_x, int pos_y, Color color)
-        : _pos_x(pos_x), _pos_y(pos_y), _color(color), _radius(5) {
-            _vit_x = 1;
-            _vit_y = 0;
-            _alt_vitesse = 0;
-            _score = 0;
+        : _pos({(float)pos_x, (float)pos_y}), _color(color), _radius(5), _vit({1.0f,0.0f}), _score(0) {
+  	    roll_alt();
+	}
+
+        void draw() const {
+            DrawCircle(_pos.x, _pos.y, _radius, _color);
         }
 
-        void check_colision() {
-            if(_pos_x+_vit_x*_alt_vitesse >= GetScreenWidth()) {
-                _vit_x *= -1;
-            }
-
-            if(_pos_x+_vit_x*_alt_vitesse <= 0) {
-                ++_score;
-                _vit_x *= -1;
-            }
-
-            if(_pos_y*_vit_y*_alt_vitesse >= GetScreenHeight()
-            || _pos_y+_vit_y*_alt_vitesse) {
-                _vit_y *= -1;
-            }
-        }
-
-        void draw() {
-            DrawCircle(_pos_x, _pos_y, _radius, _color);
-        }
-
-        unsigned int get_score() {
+        unsigned int get_score() const {
             return _score;
         }
 
-        void move() {
-            _alt_vitesse = (rand()%10) + _score;
-            check_colision();
-            _pos_x += _vit_x * _alt_vitesse;
-            _pos_y += _vit_y;
+	void drawScore(unsigned int x, unsigned int y) const {
+	    std::stringstream st("");
+	    st << _score;
+	    DrawText(st.str().c_str(), x, y , 20, _color);
+	}
+
+        void update() {
+	    char x = 0, y = 0;
+            check_collision(&x, &y);
+	    if (x == -1) {
+		++_score;
+		roll_alt();
+	    }
+	    if (x) _vit.x *= -1;
+	    if (y) _vit.y *= -1;
+	    _pos = get_next_pos();
         }
+};
+
+class Screen {
+    public:
+	virtual void draw() = 0;
+	virtual bool should_change() = 0;
+	virtual void reset_screen() = 0;
+};
+
+class GameScreen : public Screen {
+    private:
+	Boule _boule_joueur;
+	Boule _boule_adversaire;
+
+    public:
+	GameScreen(): _boule_joueur(0, 150, GREEN), _boule_adversaire(0, 180, RED)
+	{}
+
+	void draw() override {
+            _boule_joueur.update();
+            _boule_adversaire.update();
+	    DrawText("Tour : ", 1, 0, 20, WHITE);
+	    _boule_joueur.drawScore(1, 20);
+	    _boule_adversaire.drawScore(1, 40);
+            _boule_joueur.draw();
+            _boule_adversaire.draw();
+	}
+
+	bool should_change() override {
+	    return _boule_joueur.get_score() >= 10;
+	}
+
+	void reset_screen() override {
+	    _boule_joueur = Boule(0, 150, GREEN);
+	    _boule_adversaire = Boule(0, 180, RED);
+	}
+};
+
+class ShopScreen : public Screen {
+    private:
+	int _remaining_ticks;
+    public:
+        ShopScreen() : _remaining_ticks(60*10) {}
+	void draw() override {
+	    DrawText("OMG LE SHOP", 600, 100, 10, GREEN);
+	    --_remaining_ticks;
+	}
+	bool should_change() override {
+	    return (_remaining_ticks < 0);
+	}
+	
+	void reset_screen() override {
+	    _remaining_ticks = 60*10;
+	}
 };
 
 int main(void) {
     srand(time(NULL));
     InitWindow(1200, 200, "Boulatro");
 
-    Boule boule_1 = Boule(10, 150, GREEN);
-    Boule boule_2 = Boule(10, 160, YELLOW);
-    Boule boule_3 = Boule(10, 170, RED);
-    std::stringstream st1;
-    std::stringstream st2;
-    std::stringstream st3;
+    std::vector<std::unique_ptr<Screen>> screens;
+    screens.push_back(std::make_unique<GameScreen>());
+    screens.push_back(std::make_unique<ShopScreen>());
+
     SetTargetFPS(60);
+
+    size_t current_screen = 0;
     
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText("Lap : ", 1, 0, 20, WHITE);
-        
-        st1.str("");
-        st2.str("");
-        st3.str("");
-        st1 << boule_1.get_score();
-        st2 << boule_2.get_score();
-        st3 << boule_3.get_score();
-        
-        DrawText(st1.str().c_str(), 1, 20, 20, GREEN);
-        DrawText(st2.str().c_str(), 1, 40, 20, YELLOW);        
-        DrawText(st3.str().c_str(), 1, 60, 20, RED);
-        boule_1.draw();
-        boule_3.draw();
-
-        boule_1.move();
-        boule_3.move();
-        
+	if(screens[current_screen]->should_change()) {
+	    current_screen = (current_screen+1)%2;
+	    screens[current_screen]->reset_screen();
+	}
+        screens[current_screen]->draw();
         EndDrawing();
     }
 }
